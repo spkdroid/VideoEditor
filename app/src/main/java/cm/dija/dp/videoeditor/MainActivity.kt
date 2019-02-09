c3cd1f2e92b6b417ec9cb5ce2cbe125b690cdcd6
Ramkumar Velmurugan <vrk1990@outlook.com>
2019-02-09 09:08:06 -0500

    Implemented Navigation Grap

--- app/src/main/java/cm/dija/dp/videoeditor/MainActivity.kt
+++ app/src/main/java/cm/dija/dp/videoeditor/MainActivity.kt
@@ -2,18 +2,25 @@ package cm.dija.dp.videoeditor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import cm.dija.dp.videoeditor.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navHost: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
+        navHost = supportFragmentManager
+            .findFragmentById(R.id.navHost) as NavHostFragment
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
 
 }
