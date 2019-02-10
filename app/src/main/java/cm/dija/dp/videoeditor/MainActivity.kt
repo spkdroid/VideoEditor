251c73af491224bba51cc91930c29cea9c710186
Ramkumar Velmurugan <vrk1990@outlook.com>
2019-02-09 20:11:50 -0500

    Migration to AndroidX

--- app/src/main/java/cm/dija/dp/videoeditor/MainActivity.kt
+++ app/src/main/java/cm/dija/dp/videoeditor/MainActivity.kt
@@ -1,6 +1,6 @@
 package cm.dija.dp.videoeditor
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
 
--- app/src/main/java/cm/dija/dp/videoeditor/ui/main/MainViewModel.kt
+++ app/src/main/java/cm/dija/dp/videoeditor/ui/main/MainViewModel.kt
@@ -1,6 +1,6 @@
 package cm.dija.dp.videoeditor.ui.main
 
-import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
