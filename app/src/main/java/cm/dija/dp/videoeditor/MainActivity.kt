7359caac1690cf4b0381ef9a67751c960baab4e1
Ramkumar Velmurugan <vrk1990@outlook.com>
2023-11-22 14:29:43 -0500

    Navigation graph integrated

--- app/src/main/java/cm/dija/dp/videoeditor/MainActivity.kt
+++ app/src/main/java/cm/dija/dp/videoeditor/MainActivity.kt
@@ -2,10 +2,17 @@ package cm.dija.dp.videoeditor
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navHost: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navHost = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/cm/dija/dp/videoeditor/viewmodel/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package cm.dija.dp.videoeditor.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
