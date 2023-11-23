4a4126968cc2aaf4473becf2140ca07e1168fe77
Ramkumar Velmurugan <vrk1990@outlook.com>
2023-11-23 18:49:34 -0500

    Basefragment refactor

--- app/src/main/java/cm/dija/dp/videoeditor/di/BaseFragment.kt
+++ app/src/main/java/cm/dija/dp/videoeditor/di/BaseFragment.kt
@@ -2,7 +2,10 @@ package cm.dija.dp.videoeditor.di
 
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import cm.dija.dp.videoeditor.viewmodel.DashboardViewModel
 
 @AndroidEntryPoint
-open class BaseFragment:Fragment() {
-}
+open class BaseFragment:Fragment()
